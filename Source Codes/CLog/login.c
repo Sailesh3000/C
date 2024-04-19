b4825fdfdadc0138a55323a6f0578d1c29a54626
Sailesh <chandrasailesh30@gmail.com>
2024-04-19 11:30:06 +0530

    change code

--- Source Codes/CLog/login.c
+++ Source Codes/CLog/login.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
+#include <conio.h>
 #include <string.h>
 
-int main() {
+int main()
+{
+    char ch;
     char pass[20];
     char saved_pass[20];
 start:
     printf("Enter your Password: ");
-    fgets(pass, sizeof(pass), stdin);
-    pass[strcspn(pass, "\n")] = 0; // Remove trailing newline if present
+    int i = 0;
+    while (1)
+    {
+        ch = getch();
+        if (ch == 13)
+        { // ASCII Code for Enter
+            pass[i] = '\0';
+            break;
+        }
+        else
+        {
+            pass[i++] = ch;
+            printf("*");
+        }
+    }
 
-    printf("\nYou Entered: %s\n", pass);
+    printf("\nYou Entered : %s\n", pass);
 
     FILE *ptr;
     ptr = fopen("password.txt", "r");
@@ -19,15 +35,18 @@ start:
         return 1;
     }
 
-    fscanf(ptr, "%19s", saved_pass);
+   fscanf(ptr, "%19s", saved_pass);
     fclose(ptr);
 
-    printf("Password: %s ", saved_pass);
+    printf("Password : %s ", saved_pass);
 
     int l = strcmp(pass, saved_pass);
-    if (l == 0) {
+    if (l == 0)
+    {
         printf("\n\n******LOGIN SUCCESSFUL******\n\n\n");
-    } else {
+    }
+    else
+    {
         printf("\n\n******INCORRECT PASSWORD******\nPlease Try again...\n\n");
         goto start;
     }
