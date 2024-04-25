e7ac9288e417bdd0b2c915c649c4427dcd44ac02
Sailesh <chandrasailesh30@gmail.com>
2024-04-25 19:00:18 +0530

    Included signup

--- Source Codes/CLog/loginus.c
+++ Source Codes/CLog/loginus.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "sqlite3.h"
+#include <conio.h> // for _getch
+
+#define MAX_LENGTH 100
 
 int main() {
-    char pass[20];
-    char user_name[20];
+    char pass[MAX_LENGTH];
+    char user_name[MAX_LENGTH];
 
     sqlite3 *db;
     int rc = sqlite3_open("login.db", &db); // Open or create a SQLite database named login.db
@@ -15,17 +19,12 @@ int main() {
     }
 
 user:
-    printf("Enter Username: \n");
-    fgets(user_name, sizeof(user_name), stdin);
-    user_name[strcspn(user_name, "\n")] = '\0'; // Remove newline character if present
-
-    // Clear input buffer
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
+    printf("Enter Username: ");
+    scanf("%s", user_name);
 
     // Prepare the SQL statement to select the username and password for the given username
     sqlite3_stmt *stmt;
-    char sql[100];
+    char sql[MAX_LENGTH];
     sprintf(sql, "SELECT username, password FROM users WHERE username='%s'", user_name);
 
     rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0); // Prepare the SQL statement
@@ -44,23 +43,43 @@ user:
         const unsigned char *saved_pass = sqlite3_column_text(stmt, 1);
 
         printf("Enter your Password: ");
-        fgets(pass, sizeof(pass), stdin);
-        pass[strcspn(pass, "\n")] = '\0'; // Remove newline character if present
+        int i = 0;
+        char ch;
+        while ((ch = _getch()) != '\r' && i < MAX_LENGTH - 1) {
+            pass[i++] = ch;
+            printf("*");
+        }
+        pass[i] = '\0'; // Null-terminate the password string
 
-        int compare_user = strcmp(user_name, (const char *)saved_user);
-        if (compare_user == 0) {
-            if (saved_pass != NULL && strcmp(pass, (const char *)saved_pass) == 0) {
-                printf("\n\n******LOGIN SUCCESSFUL******\n\n\n");
-            } else {
-                printf("\n\n******INCORRECT PASSWORD******\nPlease Try again...\n\n");
-                goto user;
-            }
+        if (strcmp(user_name, (const char *)saved_user) == 0 && strcmp(pass, (const char *)saved_pass) == 0) {
+            printf("\n\n******LOGIN SUCCESSFUL******\n\n\n");
         } else {
-            printf("User not found\n");
+            printf("\n\n******INCORRECT PASSWORD******\nPlease Try again...\n\n");
             goto user;
         }
     } else {
         printf("User not found\n");
+        printf("Do you want to sign up? (Y/N): ");
+        char answer;
+        scanf(" %c", &answer);
+        if (answer == 'Y' || answer == 'y') {
+            // Sign-up process
+            printf("Enter your Password: ");
+            int i = 0;
+            char ch;
+            while ((ch = _getch()) != '\r' && i < MAX_LENGTH - 1) {
+                pass[i++] = ch;
+                printf("*");
+            }
+            pass[i] = '\0'; // Null-terminate the password string
+            sprintf(sql, "INSERT INTO users (username, password) VALUES ('%s', '%s')", user_name, pass);
+            rc = sqlite3_exec(db, sql, 0, 0, 0);
+            if (rc != SQLITE_OK) {
+                fprintf(stderr, "Error inserting data: %s\n", sqlite3_errmsg(db));
+            } else {
+                printf("\nSign up successful!\n");
+            }
+        }
         goto user;
     }
 
